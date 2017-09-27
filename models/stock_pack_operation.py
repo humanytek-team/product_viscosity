2c1311120dbede08e627661e81578d0b928833ff
Moisés Alejandro Navarro Presas <moisalejandro@gmail.com>
2017-09-27 18:04:51 -0500

    [IMP] auto viscosity

--- models/stock_pack_operation.py
+++ models/stock_pack_operation.py
@@ -8,7 +8,10 @@ class StopckPackOperation(models.Model):
         related="product_id.product_tmpl_id.check_viscosity",
     )
     viscosity = fields.Float(
+        compute='_get_viscosity',
+        default=lambda self: self.product_id.product_tmpl_id.viscosity,
         digits=(6, 4),
+        store=True,
     )
     kg = fields.Float(
         digits=(6, 4),
@@ -20,3 +23,10 @@ class StopckPackOperation(models.Model):
         for r in self:
             if r.viscosity > 0:
                 r.product_qty = r.viscosity*r.kg
+
+    @api.multi
+    @api.depends('product_id')
+    def _get_viscosity(self):
+        for r in self:
+            if not r.viscosity:
+                r.viscosity = r.product_id.product_tmpl_id.viscosity
